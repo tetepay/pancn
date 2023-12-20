6d447efaf529ec9f0d0423fd7818bac41c14fd01
tetepay <129178480+tetepay@users.noreply.github.com>
2023-12-20 17:18:28 +0800

    Add files via upload

---
+++ vfm-admin/users/users-new.php
@@ -0,0 +1,20 @@
+<?php
+
+ $newusers = array (
+  0 => 
+  array (
+    'name' => '565623232',
+    'pass' => '$1$JpgL42la$FgNX9weawyuaZLACK7iyX/',
+    'email' => '565262323@qq.com',
+    'date' => '2023-12-20',
+    'key' => 'b61f2937d8b7e9c184d6280470355ac5',
+  ),
+  1 => 
+  array (
+    'name' => '565623232966',
+    'pass' => '$1$FbyL6GvZ$9eiMitzT8GjErDc5NyN.V/',
+    'email' => 'freepayzz@163.com',
+    'date' => '2023-12-20',
+    'key' => 'ec8ce8788ed5fc8de30516cb8aaf69fc',
+  ),
+);
---
+++ vfm-admin/users/users.php
@@ -0,0 +1,18 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'tetepay',
+    'pass' => '$1$Hplri6V3$mpTbCODnRRNSexu/KbU0z0',
+    'role' => 'superadmin',
+    'email' => 'freepayzz@163.com',
+  ),
+  1 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$QIDtk3N3$Vr8u40/af9yzuQkAdYqOU/',
+    'role' => 'user',
+    'email' => '123456@qq.com',
+  ),
+);
